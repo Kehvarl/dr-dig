21034453ddd140a82bdd848d2dcfaf33b1e932a7
kehvarl <kehvarl@kehvarl.com>
2026-06-18 22:00:43 -0700

    use my sprite class like it was intended. I think.

--- app/game.rb
+++ app/game.rb
@@ -1,14 +1,7 @@
 module Main
   class Game
     def initialize vars={}
-      @player = AnimSprite.new(640, 576, true)
-      @player.path = "sprites/ball.png"
-      @player.tile_w = 32
-      @player.tile_h = 32
-      @player.pose_list = {
-        #Name: [Row, Frames, Repeat, [Next Anim Options]]
-        idle: [0,8,1,[:idle]],
-      }
+      @player = Player.new(640, 576, true)
 
       @world_w = 1280/16
       @world_h = 576/16
---
+++ app/game_sprites.rb
@@ -0,0 +1,34 @@
+require 'app/sprite.rb'
+module Main
+
+  class Player < AnimSprite
+    def initialize (x,y, is_player)
+      super(x,y,is_player)
+      @path= "sprites/ball.png"
+      @w= 64
+      @h= 64
+      @tile_w= 32
+      @tile_h= 32
+      @vx = 2
+      @vy = 2
+
+      @current_pose = :idle
+      @pose_list = {
+        #Name: [Row, Frames, Repeat, [Next Anim Options]]
+        idle: [0,8,1,[:idle]],
+        in_air: [1,2,1, [:idle]],
+        jump:   [2,3,1, [:idle]],
+        land:   [3,4,1, [:idle]],
+        shoot:  [4,4,1, [:idle]],
+        jump2:  [5,6,1, [:idle]],
+        hurt:   [6,4,1, [:idle]],
+        coin:   [7,3,1, [:idle]],
+        dash:   [8,1,1, [:idle]],
+        walk:    [9,4,1, [:idle]]        
+      }
+
+      @frame_duration = 10
+      @frame_delay = 10
+    end
+  end
+end
