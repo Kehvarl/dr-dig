4e83d9433773f47a2d2a32a7edc79255d5efff57
Kehvarl <Kehvarl@Kehvarl.com>
2026-06-28 18:33:05 -0700

    Trying to rebuild some of the basics to get new inspiration

--- app/game.rb
+++ app/game.rb
@@ -1,47 +1,54 @@
 module Main
   class Game
     def initialize vars={}, args
+      @background = build_background()
+      @grid = build_grid()
+      @tile_size = 32
+    end
 
-      @world_w = 1280/16 #40
-      @world_h = 576/16 #18
-      @world = GameMap.new()
-      @world.build_playfield
+    def build_background
+      out = []
+      out << {x:0, y:576, w:1280, h:144, r:0, g:0, b:216}.solid!
+      out << {x:0, y:0, w:1280, h:576, r:0, g:0, b:0}.solid!
+      out
+    end
 
-      @player = Player.new(40,18, 32, 32, true)
-      @player.move_to(20,18, :idle)
+    def create_tile(x, y, visible=true, block_movement=true, destroyable=true, hp=3)
+      {x:x, y:y, visible:visible, block_movement:block_movement, destroyable:destroyable, hp:hp}
+    end
+
+    def build_grid(h=18, w:40)
+      grid = []
+      (0..h-1).each do |ph|
+        (0..w-1).each do |pw|
+          grid << create_tile(pw + 0, ph + 0, true, true, true)
+        end
+      end
+      grid
+    end
+
+    def render_grid
+      out = []
+      @grid.each do |p|
+        a = 255
+        if not p.visible
+          a = 0
+        end
+        out << {x:p.x*@tile_size, y:p.y*@tile_size,
+                w:@tile_size, h:@tile_size, a:a,
+                path: "sprites/square/gray.png"}.sprite!
+      end
+      out
     end
 
     def render
       out = []
-      out << {x:0, y:576, w:1280, h:144, r:0, g:0, b:216}.solid!
-      out << {x:0, y:0, w:1280, h:576, r:0, g:0, b:0}.solid!
-      out << @world.draw_playfield
-      out << @player
+      out << @background
+      out << render_grid()
       out
     end
 
-    def jump
-      if !@world.tile_blocked?(@player.map.x, @player.map.y)
-        puts @player.map
-        @player.move_to(@player.map.x, @player.map.y + 1, :idle)
-      end
-    end
-
-    def fall
-      if ! @player.moving
-        puts @player.map
-        if !@world.tile_blocked?(@player.map.x, @player.map.y - 1)
-          @player.move_to(@player.map.x, @player.map.y - 1, :idle)
-        end
-      end
-    end
-
-    def tick args
-      @player.tick(args, [], true)
-      fall()
-      if args.inputs.keyboard.space
-        jump()
-      end
+    def tick (args)
     end
   end
 end
